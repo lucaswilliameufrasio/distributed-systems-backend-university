c2fe10bb975fdbaf9060655d54503aa07db7b6df
lucaswilliameufrasio <lucaswilliameufrasio@gmail.com>
2019-07-02 01:40:34 -0300

    Alterações e Melhorias no GarcomController

--- app/Http/Controllers/GarcomController.php
+++ app/Http/Controllers/GarcomController.php
@@ -2,24 +2,40 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\ListaMesas;
 use App\Models\Mesa;
 use Illuminate\Http\Request;
-use App\Events\ListaMesas;
 
 class GarcomController extends Controller
 {
-    public function mesas(){
-
+    public function mesas()
+    {
         $params = Mesa::orderBy('id', 'asc')->get();
+
         return response()->json(
             $params, 200);
     }
-    public function atualizaStatusMesa(Request $request, $id){
+
+    public function atualizaStatusMesa(Request $request, $id)
+    {
+        //Atualiza o status da mesa para true, ativa
         Mesa::where('id', $id)->update([
-            'status' =>  $request['status'],
+            'status' => $request['status'],
         ]);
+
+        //Cria um pedido para a mesa correspondente
+        Pedido::create([
+            'statusPedido' => 'Em produção',
+            'valor' => 0,
+            'finalizado' => false,
+            'mesas_id' => $id,
+        ]);
+
         $params = Mesa::orderBy('id', 'asc')->get();
+
+        //Dispara um evento após atualizar o status da mesa
         event(new ListaMesas($params));
+
         return response()->json(
             $params, 200);
     }
