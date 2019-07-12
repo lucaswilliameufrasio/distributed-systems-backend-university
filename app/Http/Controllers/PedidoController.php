5dcf0edbb8d9068f5e02e9040506263e35681c6b
lucaswilliameufrasio <lucaswilliameufrasio@gmail.com>
2019-07-11 22:40:14 -0300

    Alteração no controlador de Pedidos

--- app/Http/Controllers/PedidoController.php
+++ app/Http/Controllers/PedidoController.php
@@ -2,11 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Pedido;
 use App\Models\ItensPedido;
+use App\Models\Pedido;
 use App\Models\Produto;
-use Illuminate\Http\Request;
 use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PedidoController extends Controller
 {
@@ -14,11 +15,27 @@ class PedidoController extends Controller
     {
         $pedido = Pedido::where('finalizado', false)->where('mesas_id', $id)->whereDate('created_at', Carbon::today())->first();
 
-        $itensPedidos = ItensPedido::where('pedidos_id', $pedido->id)->get();
+        if (!$pedido) {
+            return response()->json([
+                'data' => 'Não há pedidos para nesta mesa.',
+            ], 200,
+                ['Content-type' => 'application/json; charset=utf-8'], JSON_UNESCAPED_UNICODE);
+        }
 
+        $itensPedidos = DB::table('itensPedidos')
+            ->select('itensPedidos.*', 'pedidos.*', 'produtos.*', 'itensPedidos.id as itensid')
+            ->where('pedidos.mesas_id', $id)
+            ->where('pedidos.finalizado', false)
+            ->join('pedidos', 'pedidos.id', '=', 'itensPedidos.pedidos_id')
+            ->join('produtos', 'produtos.id', '=', 'itensPedidos.produtos_id')
+            ->get();
+
+        $pedido = Pedido::where('pedidos.mesas_id', $id)->where('pedidos.finalizado', false)->get();
+        $produtos = Produto::get();
         return response()->json([
             'pedido' => $pedido,
             'itens' => $itensPedidos,
+            'produtos' => $produtos,
         ], 200,
             ['Content-type' => 'application/json; charset=utf-8'], JSON_UNESCAPED_UNICODE);
     }
